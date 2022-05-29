04291b86d353e89b93c79ae1f8167b2835f4f2ae
mriise <mark.riise26@gmail.com>
2022-05-28 18:50:02 -0700

    things are workinggit add .

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 #![no_std]
 #![no_main]
 
-// use defmt_rtt as _;
+use defmt_rtt as _;
 // use panic_halt as _;
 use panic_probe as _;
 
@@ -16,16 +16,17 @@ pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 // https://crates.io/crates/switch-hal
 
-#[rtic::app(device = crate::hal::pac, peripherals = true, dispatchers = [PIO0_IRQ_0])] // extra PIO0_IRQ_1, PIO1_IRQ_0
+#[rtic::app(device = crate::hal::pac, peripherals = true, dispatchers = [XIP_IRQ])] // extra PIO0_IRQ_1, PIO1_IRQ_0
 mod app {
     use crate::hal;
     // use cortex_m::prelude::*;
     // use hal::prelude::*;
+    use defmt::*;
     // use hal::{gpio::bank0::Gpio12, timer::CountDown};
 
-    // use embedded_hal::digital::v2::{InputPin, OutputPin};
+    use embedded_hal::digital::v2::{InputPin, OutputPin};
 
-    // use embedded_time::{duration::units::*, fixed_point::FixedPoint};
+    use embedded_time::{duration::units::*, fixed_point::FixedPoint};
 
     use hal::{
         clocks::{init_clocks_and_plls, Clock},
@@ -53,8 +54,8 @@ mod app {
 
     #[local]
     struct Local {
-        // debug_led: Pin<bank0::Gpio25, Output<PushPull>>,
-        // debug_delay: cortex_m::delay::Delay,
+        debug_led: Pin<bank0::Gpio25, Output<PushPull>>,
+        debug_delay: cortex_m::delay::Delay,
     }
 
     #[init]
@@ -75,28 +76,30 @@ mod app {
         .ok()
         .unwrap();
 
-        let monotonic = rp2040_monotonic::Rp2040Monotonic::new(ctx.device.TIMER);
+        let monotonic = Rp2040Monotonic::new(ctx.device.TIMER);
         // let mut alarm = timer.alarm_0().unwrap();
         // alarm.schedule(TIMER_INTERVAL.microseconds()).ok();
         // TODO
         // alarm.enable_interrupt();
 
         // move timer into static lifetime early so Ws2812 can use it
-        // let debug_delay =
-        //     cortex_m::delay::Delay::new(ctx.core.SYST, clocks.system_clock.freq().integer());
+        // TODO reput this in
 
-        // let sio = Sio::new(ctx.device.SIO);
-        // let pins = hal::gpio::Pins::new(
-        //     ctx.device.IO_BANK0,
-        //     ctx.device.PADS_BANK0,
-        //     sio.gpio_bank0,
-        //     &mut resets,
-        // );
+        let debug_delay =
+            cortex_m::delay::Delay::new(ctx.core.SYST, clocks.system_clock.freq().integer());
+
+        let sio = Sio::new(ctx.device.SIO);
+        let pins = hal::gpio::Pins::new(
+            ctx.device.IO_BANK0,
+            ctx.device.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut resets,
+        );
 
         // Debug LEDs
         // let green = pins.gpio16.into_readable_output();
         // let red = pins.gpio17.into_readable_output();
-        // let blue = pins.gpio25.into_push_pull_output();
+        let blue = pins.gpio25.into_push_pull_output();
 
         // // neopixel
         // let pixel_power = pins.gpio11;
@@ -112,41 +115,43 @@ mod app {
         //     timer.count_down(),
         // );
 
+        tick::spawn().ok();
         // // watchdog with low priority of 1kHz
         // cortex_m::prelude::_embedded_hal_watchdog_WatchdogEnable::start(
         //     &mut watchdog,
         //     1_000.microseconds(),
         // );
 
-        // info!("init finished");
+        info!("init finished");
         (
             Shared {},
             Local {
-                // debug_led: blue,
-                // debug_delay,
+                debug_led: blue,
+                debug_delay,
             },
             init::Monotonics(monotonic),
         )
     }
 
-    #[idle]
-    fn idle(_ctx: idle::Context) -> ! {
-        loop {}
+    #[idle(shared = [], local = [debug_led, debug_delay])]
+    fn idle(ctx: idle::Context) -> ! {
+        let delay = ctx.local.debug_delay;
+        let debug_led = ctx.local.debug_led;
+        loop {
+            info!("on!");
+            debug_led.set_high().unwrap();
+            delay.delay_ms(500);
+            info!("off!");
+            debug_led.set_low().unwrap();
+            delay.delay_ms(500);
+        }
     }
 
-    // #[idle(shared = [], local = [debug_led, debug_delay])]
-    // fn idle(ctx: idle::Context) -> ! {
-    //     let delay = ctx.local.debug_delay;
-    //     let debug_led = ctx.local.debug_led;
-    //     loop {
-    //         info!("on!");
-    //         debug_led.set_high().unwrap();
-    //         delay.delay_ms(500);
-    //         info!("off!");
-    //         debug_led.set_low().unwrap();
-    //         delay.delay_ms(500);
-    //     }
-    // }
+    #[task]
+    fn tick(_: tick::Context) {
+        info!("Tick");
+        tick::spawn_after(1_000_u64.millis()).ok();
+    }
 
     // #[task(shared = [], local=[counter: u8 = 0, prev: u8 = 0], priority = 1)]
     // fn blink(ctx: blink::Context) {}
